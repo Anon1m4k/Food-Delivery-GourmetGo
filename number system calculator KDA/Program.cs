91424c49dac223d74ce938842fe84778607b7cbd
Катя <20katerina0060@gmail.com>
2025-02-22 11:56:14 +0300

    Подключили базу данных

--- number system calculator KDA/Program.cs
+++ number system calculator KDA/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using number_system_calculator_KDA.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+//Настройка подключения к базе данных 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("FoodDeliveryDb")));
 
 var app = builder.Build();
 
@@ -19,3 +26,4 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
