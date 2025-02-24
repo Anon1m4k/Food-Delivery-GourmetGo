ccf6f08f5c2a3d964ee240e206ab42a638475c9d
Катя <20katerina0060@gmail.com>
2025-02-24 16:27:56 +0300

    Создали модели

---
+++ number system calculator KDA/Model/Basket.cs
@@ -0,0 +1,10 @@
+﻿namespace number_system_calculator_KDA.Model
+{
+    public class Basket
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
