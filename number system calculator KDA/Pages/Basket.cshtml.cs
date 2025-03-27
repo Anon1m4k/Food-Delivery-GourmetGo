b66957f4ed230c7d7bea32498219fbbb655e23df
Anon1m4k <kirilvalera2@gmail.com>
2025-03-27 09:38:32 +0300

    Доделали корзину

--- number system calculator KDA/Pages/Basket.cshtml.cs
+++ number system calculator KDA/Pages/Basket.cshtml.cs
@@ -9,7 +9,6 @@ namespace number_system_calculator_KDA.Pages
     public class BasketModel : PageModel
     {
         private readonly ApplicationDbContext _context;
-
         public BasketModel(ApplicationDbContext context)
         {
             _context = context;
@@ -21,12 +20,19 @@ namespace number_system_calculator_KDA.Pages
         }
         public IActionResult OnPostDelete(int id)
         {
-            var dish = _context.Dishs.Find(id);
-            if (dish != null)
+            var basketItem = _context.Baskets.Find(id);
+            if (basketItem != null)
             {
-                _context.Dishs.Remove(dish);
+                _context.Baskets.Remove(basketItem);
                 _context.SaveChanges();
             }
+            return RedirectToPage();
+        }
+        public IActionResult OnPostClear()
+        {
+            var baskets = _context.Baskets.ToList();
+            _context.Baskets.RemoveRange(baskets);
+            _context.SaveChanges();
             return RedirectToPage();
         }
     }
