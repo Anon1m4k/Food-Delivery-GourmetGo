1f9fb1b3e6cf0b9912f603d594d3e5d7deeef8eb
Anon1m4k <kirilvalera2@gmail.com>
2025-04-26 12:18:32 +0300

    Не работает

---
+++ number system calculator KDA/Model/LoginModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace number_system_calculator_KDA.Model
+{
+    public class LoginModel
+    {
+        public ClaimsIdentity? Username { get; internal set; }
+        public bool RememberMe { get; internal set; }
+    }
+}
